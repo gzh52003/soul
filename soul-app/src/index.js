2e9ee0dbaed4231c843fbacfdde0ff448c6abceb
DDboom <a499947285@qq.com>
2020-09-22 15:50:47 +0800

    初始化,未配置路由

--- soul-app/src/index.js
+++ soul-app/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {HashRouter,BrowserRouter} from 'react-router-dom'
+
+const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
